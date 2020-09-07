287518a2c25c8493fdaca784d1793f9fe3cd8f6f
Truls <fanthoft@gmail.com>
2020-09-07 21:03:31 +0200

    add case for special characters

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -6,11 +6,16 @@ def caesar_cipher(str, shift)
     str.each_char { |c|
         # find the index of a character in the alphabet and shift it over, mod the size of the alphabet
         idx = letters.find_index(c)
-        idx += shift
-        shifted_string += letters[idx % letters.length]
+        if idx == nil
+            shifted_string += c
+        else
+            idx += shift
+            shifted_string += letters[idx % letters.length]
+        end
     }
     
     return shifted_string
 end
 
 puts caesar_cipher("puluka", 13)
+puts caesar_cipher("What a string!", 5)
